d7cc7b320d3c3f1de9b7c4860dfaa1086d65380d
nuiacdoechare <nchareca@uade.edu.ar>
2022-10-21 11:23:18 -0300

    Test cases y funciones test

---
+++ test_pytest/test_hello_world.py
@@ -0,0 +1,11 @@
+import pytest
+
+def test_hello_world():
+    assert 5 + 1 == 6
+
+def test_hello_world_2():
+    assert 5 == "5"
+
+
+# falta instalar pytest y selenium en el venv,
+# para probar las funciones
